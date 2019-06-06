253fcdf196fa2945ffbf2f84e5d84be7fe10bdc2
ÖnderGüler <onder031@gmail.com>
2019-06-06 23:04:49 +0300

    ssss

--- VisualEffect/ViewController.swift
+++ VisualEffect/ViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var visualEffectView: UIVisualEffectView!
+    @IBOutlet var addItemView: UIView!
+    var effect:UIVisualEffect!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        effect = visualEffectView.effect
+        visualEffectView.effect = nil
+        
+        addItemView.layer.cornerRadius = 5
+        
     }
-
-
+    
+    func animateIn(){
+        self.view.addSubview(addItemView)
+        addItemView.center = self.view.center
+        
+        addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        addItemView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4){
+            self.visualEffectView.effect = self.effect
+            self.addItemView.alpha = 1
+            self.addItemView.transform = CGAffineTransform.identity
+            
+            
+        }
+    }
+    func animetOut () {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.addItemView.alpha = 0
+            
+            self.visualEffectView.effect = nil
+        }) { (success:Bool) in
+            self.addItemView.removeFromSuperview()
+        }
+    
+    }
+    @IBAction func addItem(_ sender: Any) {
+        animateIn()
+    }
+    
+    @IBAction func dismissPopUp(_ sender: Any) {
+        animetOut()
+    }
 }
 
